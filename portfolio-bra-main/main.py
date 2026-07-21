d16064672810c526d4124ba3bfc813bc5271fdfe
xarp2000 <138525203+xarp2000@users.noreply.github.com>
2026-07-21 20:24:07 -0300

    Add files via upload

---
+++ portfolio-bra-main/main.py
@@ -0,0 +1,22 @@
+clea# Importar
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# Conteúdo da página
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Habilidades Dinâmicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
